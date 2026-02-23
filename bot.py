e9f57ab32ae670c8c3d5624ff5cffc5c5e398c63
36rrcom <36rr.com@gmail.com>
2026-02-23 21:09:19 +0300

    Update app.run to use environment variables for host and port

--- bot.py
+++ bot.py
@@ -28,5 +28,7 @@ def reply():
     msg.body(analyze(incoming))
     return str(response)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
